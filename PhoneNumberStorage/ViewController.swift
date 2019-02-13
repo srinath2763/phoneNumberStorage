e512432548d39bf1889be80d8f4e9b6cfab46434
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-13 11:26:04 -0600

    Working Update Button

--- PhoneNumberStorage/ViewController.swift
+++ PhoneNumberStorage/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var phone: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if isKeyPresentInUserDefaults(key: "name") && isKeyPresentInUserDefaults(key: "phone"){
+            saveButton.setTitle("update", for: .normal)
+        }
+        
     }
-
-
+    
+    func isKeyPresentInUserDefaults(key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: key) != nil
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        UserDefaults.standard.set(name.text, forKey: "name")
+            UserDefaults.standard.set(phone.text, forKey: "phone")
+    }
+    
 }
 
