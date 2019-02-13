28d08d93a185b75447307493ad764b8e4cf7cb73
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-13 10:46:48 -0600

    Initial Commit

---
+++ PhoneNumberStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneNumberStorage
+//
+//  Created by IMCS2 on 2/13/19.
+//  Copyright © 2019 IMCS2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
